8e0dd39c54c31402aea71eff4b5151967f2df015
姚文强 <yaowenqiang111@163.com>
2022-08-10 23:20:52 +0800

    add sigleton  pattern via allocator demo

---
+++ sigleton_allocator.py
@@ -0,0 +1,25 @@
+import random
+class Database:
+    _instance = None
+
+    def  __init__(self):
+        id = random.randint(1, 101)
+        print('id = ', id)
+        print('Loading a database from file')
+
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Database, cls)\
+                .__new__(cls, *args, **kwargs)
+
+        return cls._instance
+
+
+
+
+if __name__ == '__main__':
+    d1 = Database()
+    d2 = Database()
+
+    print(d1 == d2)
